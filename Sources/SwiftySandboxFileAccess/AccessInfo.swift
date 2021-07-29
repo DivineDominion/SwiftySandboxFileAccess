de70d4a621942c30238a101e0f24056ed75697fe
Rob Jonson <rob@hobbyistsoftware.com>
2021-07-29 13:27:20 +0100

    make AccessInfo public

--- Sources/SwiftySandboxFileAccess/AccessInfo.swift
+++ Sources/SwiftySandboxFileAccess/AccessInfo.swift
@@ -10,10 +10,10 @@ import Foundation
 /// Info on what access was available to access the required file
 public struct AccessInfo {
     //Note -this is the url of the bookmark actually used to access your file. It may be a parent of the file you want.
-    let securityScopedURL:URL?
-    let bookmarkData:Data?
-    let permissions:Permissions
+    public let securityScopedURL:URL?
+    public let bookmarkData:Data?
+    public let permissions:Permissions
     
-    static let empty = AccessInfo(securityScopedURL: nil,bookmarkData: nil,permissions: .none)
+    public static let empty = AccessInfo(securityScopedURL: nil,bookmarkData: nil,permissions: .none)
 }
 
